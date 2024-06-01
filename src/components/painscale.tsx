dbc50f1c2c65a2f3569b64a474e8d60783b67f06
Muhammad Ashir <mashir670@gmail.com>
2024-06-01 18:07:43 +0500

    painscale ui complete

--- src/components/painscale.tsx
+++ src/components/painscale.tsx
@@ -1,32 +1,79 @@
-import React, { useEffect } from 'react'
 import { UseFormRegister } from 'react-hook-form'
 import { FormSchema } from './form'
 import { z } from 'zod'
 import "./painscale.css"
+import Emojies from './emojies'
 
 export default function Painscale(props: {
   register: UseFormRegister<z.infer<typeof FormSchema>>
 }) {
 
-  useEffect(() => {
-    const lines = document.getElementsByClassName('hori-line')
-    lines.item(0)?.setAttribute('style', 'width: 50%')
-    lines.item(10)?.setAttribute('style', 'width: 50%; left: 0')
-  }, [])
+  const text = {
+    0: {
+      content: 'No pain',
+      columnStart: 1,
+      columnEnd: 2
+    },
+    1: {
+      content: 'Mild',
+      columnStart: 2,
+      columnEnd: 4
+    },
+    2: {
+      content: 'Moderate',
+      columnStart: 5,
+      columnEnd: 7
+    },
+    3: {
+      content: 'Severe',
+      columnStart: 8,
+      columnEnd: 10,
+    },
+    4: {
+      content: 'Very severe',
+      columnStart: 10,
+      columnEnd: 10
+    },
+  }
 
   return (
     <div className='painscale-container'>
-      
+
+      <div className='emojies-container'>
+        <Emojies />
+      </div>
+
+      <div className='colored-lines-container'>
+        <div className="colored-line" style={{borderColor:"#22c55e", gridColumnStart: 2, gridColumnEnd: 4}}></div>
+        <div className="colored-line" style={{borderColor:"#facc15", gridColumnStart: 5, gridColumnEnd: 7}}></div>
+        <div className="colored-line" style={{borderColor:"#b91c1c", gridColumnStart: 8, gridColumnEnd: 10}}></div>
+      </div>
+
+      <div className='text-container'>
+        {
+          Object.entries(text).map(([key, value]) => (
+            <div key={key} style={{gridColumnStart: value.columnStart, gridColumnEnd: value.columnEnd}}>
+              {value.content}
+            </div>
+          ))
+        }
+      </div>
+
       <div className='vert-lines-container'>
+        <div className='hori-line'></div>
         {Array.from({length: 11}).map((_, e) => (
-          <div className='line-radio-container'>
-            <div key={e} className='vert-line' />
-            <div className='hori-line'/>
-            <div key={e} className='painscale-radio'>
+          <div key={e} className='vert-line'></div>
+        ))}
+      </div>
+      
+      <div className='painscale-radio-container'>
+        {Array.from({length: 11}).map((_, e) => (
+          <div key={e} className='line-radio-container'>
+            <div className='painscale-radio'>
               <label>
                 <input
                   type="radio"
-                  value="option3"
+                  value={e}
                 />
                 {e}
               </label>
@@ -34,8 +81,6 @@ export default function Painscale(props: {
           </div>
         ))}
       </div>
-      
-
     </div>
   )
 }
